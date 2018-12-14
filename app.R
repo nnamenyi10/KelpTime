ac457036d960b9adb4e3ad4dc786b251aa92d4ac
Jason Griffith <griffith.jr11@gmail.com>
2018-12-13 19:27:43 -0500

    added filter by dates

--- app.R
+++ app.R
@@ -22,10 +22,11 @@ ui <- fluidPage(
                   "Option2" = "dref2",
                   "Option3" = "dref3")),
     
-    sliderInput("bogustag4", "Slider Title", 
-                min = 0, max = 100, value = 50, step = 1),
+    sliderInput("dateFilter", "Filter by date:", 
+                min = min(ssl["Start"]), max = max(ssl["Start"]), 
+                value = c(min(ssl["Start"]),max(ssl["Start"])), step = 1, sep=""),
     
-    checkboxInput("legendtag", "Show Legend", TRUE),
+    checkboxInput("legendtag", "Show Legend", FALSE),
     
     actionButton("bogustag", "Bogus Button")
   )
@@ -35,9 +36,9 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  rawData <- read.csv("CleanData.csv", stringsAsFactors=FALSE)
-  site_slopes <- read.csv("eco_sites.csv", stringsAsFactors=FALSE) %>%
-    filter(Period == "1900-2015")
+  rawData <- read.csv("CleanDataWithRegions.csv", stringsAsFactors=FALSE)
+  site_slopes <- read.csv("eco_sites.csv", stringsAsFactors=FALSE) # %>%
+    #filter(Period == "1900-2015")
   
   rawDataFilt <- rawData %>%
     dplyr::mutate(SiteName =  paste(Study, Site, study_ID,trajectory_ID,sep="-")) %>%
@@ -60,29 +61,36 @@ server <- function(input, output, session) {
   
   qpal <- colorNumeric(palette = c("red", "purple", "blue"), domain = ssl$mean, n = 11)
   
+  filtered = reactive({
+    updateCheckboxInput(session, "legendtag", value = FALSE)
+    ssl[ssl$Start >= input$dateFilter[1] & ssl$End <= input$dateFilter[2], ]
+  })
+  
+  
   output$mymap <- renderLeaflet({
     leaflet() %>% 
       addTiles(options = providerTileOptions(minZoom = 1, maxZoom = 10)) %>%
       addCircleMarkers(
-        data = ssl, 
+        data = filtered(), 
         lng = ~Longitude, 
         lat = ~Latitude,
         opacity=0.5,
         color = ~qpal(mean),
         popup = ~lab)
+    #addLegend(position = "bottomright", pal = qpal, values = ssl$mean)
   })
+  
   
   #Show the legend if the checkbox is ticked 
   observe({
     proxy <- leafletProxy("mymap")
     proxy %>% clearControls()
-    #lFormat <- labelFormat(digits = 2)
+    
+    
     if(input$legendtag) {
-      
       proxy %>% addLegend(position = "bottomright", pal = qpal, values = ssl$mean)
     }
   })  
-  
 }
 
 shinyApp(ui, server)
